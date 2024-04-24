78907faeae9c192ae208fd8dc47e0818c0ebb303
Vaibhav D <97098598+vaibhxv11@users.noreply.github.com>
2024-04-24 10:22:10 +0530

    Update openai.js

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai"
+import {OPENAI_KEY} from './constants'
 
- const OPENAI_KEY=sk-proj-oyUyjISOK8rLb8fDWHhOT3BlbkFJg6bN6zC0x2QS9DnbWrih
  const openai=new OpenAI({
     apiKey :OPENAI_KEY,
     dangerouslyAllowBrowser:true ,
